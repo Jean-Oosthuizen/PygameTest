8af79a8b54cc0a97f92d6da3d287d206dcd09b57
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2023-03-04 22:19:01 +0200

    Added functionality and conway ruleset
    The game is now functional. You build a starting state, and then press "i" in order to start applying the rules. You can interrupt the game by pressing "i" again, which then allows you to edit the field. I need to iron out a bug where a glider will hit the bottom corner of the screen and appear at the top, and I'm working on it

--- conwaysGameOfLife.py
+++ conwaysGameOfLife.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, time
 
 class cell():
     def __init__(self,colour,coordinates,size):
@@ -11,15 +11,16 @@ class cell():
         return f"This object has a colour of {self.colour}. It is at coordinates: {self.coordinates}"
 def drawScreen():
     screen.fill((0,0,0))
-    for row in range(0,len(level)):
-        for column in range(0,len(level[0])):
+    for row in range(len(level)):
+        for column in range(len(level[0])):
             if level[row][column] == "a":
                 screen.blit(cellA.name,(column*cellSize + 1, row*cellSize + 1))
             if level[row][column] == "b":
                 screen.blit(cellB.name,(column*cellSize + 1, row*cellSize + 1))
 def eventHandler():
+    global ready
     for event in events:
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and ready == False:
             pos = pygame.mouse.get_pos()
             x = -1
             y = -1
@@ -39,13 +40,47 @@ def eventHandler():
                 level[y][x] = "a"
             pygame.mixer.Sound.play(click)
         elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_i:
+                if ready == False:
+                    ready = True
+                else:
+                    ready = False
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
         elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
+def nextGeneration():
+    nextFrame = []
+    for row in range(len(level)):
+        nextRow = []
+        for column in range(len(level[0])):
+            #we have selected a tile to evaluate
+            bCount = 0
+            cell = level[row][column]
+            for i in range(1,4):
+                for j in range(1,4):
+                    try:
+                        if i == 2 and j ==2:
+                            pass
+                        elif level[row+2-i][column+2-j] == "b":
+                            bCount += 1
+                    except:
+                        pass
+            if cell == "b" and bCount in [2,3]:# a = dead, b = alive
+                note = "The cell at "+str(column+1)+"x, "+str(row+1)+"y is alive and will stay alive"
+                nextRow.append("b")
+            elif cell == "b" and bCount not in [2,3]:
+                note = "The cell at "+str(column+1)+"x, "+str(row+1)+"y is alive but will die"
+                nextRow.append("a")
+            elif cell == "a" and bCount == 3:
+                note = "The cell at "+str(column+1)+"x, "+str(row+1)+"y is dead but will come alive"
+                nextRow.append("b")
+            else:
+                nextRow.append(level[row][column])
+        nextFrame.append(nextRow)
+    return nextFrame
 
 pygame.init()
 screenSize = 500
@@ -59,9 +94,11 @@ click = pygame.mixer.Sound("click.wav")
 
 level = [["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],]
 
-
+ready = False
 while True:
-
+    if ready == True:
+        level = nextGeneration()
+        time.sleep(0.2)
     drawScreen()
     events = pygame.event.get()
     eventHandler()
