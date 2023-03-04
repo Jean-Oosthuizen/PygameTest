78d01fbefafcdfdcb77bcf3a8c4db682db9d4eaf
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2023-03-04 20:58:24 +0200

    Started trying to recreate conways game of life
    Started recreating conways game of life. This program will allow you to edit cells by clicking on them, but it does not have the "activate actions" functionality yet

---
+++ conwaysGameOfLife.py
@@ -0,0 +1,63 @@
+import pygame, sys
+
+class cell():
+    def __init__(self,colour,coordinates,size):
+        self.colour = colour
+        self.coordinates = coordinates
+        self.size = size
+        self.name = pygame.Surface((self.size,self.size))
+        self.name.fill((self.colour))
+    def __str__(self):
+        return f"This object has a colour of {self.colour}. It is at coordinates: {self.coordinates}"
+def drawScreen():
+    for row in range(0,len(level)):
+        for column in range(0,len(level[0])):
+            if level[row][column] == "a":
+                screen.blit(cellA.name,(column*cellSize + 1, row*cellSize + 1))
+            if level[row][column] == "b":
+                screen.blit(cellB.name,(column*cellSize + 1, row*cellSize + 1))
+def eventHandler():
+    for event in events:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            x = -1
+            y = -1
+            countx = 0
+            county = 0
+            while x == -1:
+                if pos[0] < (countx * cellSize):
+                    x = countx - 1
+                    while y == -1:
+                        if pos[1] < (county * cellSize):
+                            y = county - 1
+                        county +=1
+                countx +=1
+            if level[y][x] == "a":
+                level[y][x] = "b"
+            else:
+                level[y][x] = "a"
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+        elif event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+pygame.init()
+screenSize = 500
+screen = pygame.display.set_mode((screenSize,screenSize))
+
+cellSize = 50
+cellA = cell((40,40,40),(0,0),(48))
+cellB = cell((255,255,255),(0,0),(48))
+
+level = [["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a"],]
+screen.fill((0,0,0))
+
+while True:
+    drawScreen()
+    events = pygame.event.get()
+    eventHandler()
+    pygame.display.update()
