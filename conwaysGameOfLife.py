77b35caf2f65fea9faeff29c3034f479b8686606
Skunko-Hilarious <jeanoosthuizenalt@gmail.com>
2023-03-05 16:03:12 +0200

    Changed controls, added framerate/tickcount
    You now start and stop life with the spacebar. The game is now locked to a certain refresh rate so won't chew *as much* of your CPU resources. I also removed some redundant/useless code, and changed the letters that represented living and dead cells to make them easier to read.

--- conwaysGameOfLife.py
+++ conwaysGameOfLife.py
@@ -7,16 +7,14 @@ class cell():
         self.size = size
         self.name = pygame.Surface((self.size,self.size))
         self.name.fill((self.colour))
-    def __str__(self):
-        return f"This object has a colour of {self.colour}. It is at coordinates: {self.coordinates}"
 def drawScreen():
     screen.fill((0,0,0))
     for row in range(len(level)):
         for column in range(len(level[0])):
+            if level[row][column] == "d":
+                screen.blit(cellD.name,(column*cellSize + 1, row*cellSize + 1))
             if level[row][column] == "a":
                 screen.blit(cellA.name,(column*cellSize + 1, row*cellSize + 1))
-            if level[row][column] == "b":
-                screen.blit(cellB.name,(column*cellSize + 1, row*cellSize + 1))
 def eventHandler():
     global ready
     for event in events:
@@ -34,18 +32,20 @@ def eventHandler():
                             y = county - 1
                         county +=1
                 countx +=1
-            if level[y][x] == "a":
-                level[y][x] = "b"
-            else:
+            if level[y][x] == "d":
                 level[y][x] = "a"
+            else:
+                level[y][x] = "d"
             pygame.mixer.Sound.play(click)
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_i:
+            if event.key == pygame.K_SPACE:
                 if ready == False:
                     ready = True
                 else:
                     ready = False
-            if event.key == pygame.K_ESCAPE:
+            elif event.key == pygame.K_c and ready == False:
+                clearLevel()
+            elif event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 sys.exit()
         elif event.type == pygame.QUIT:
@@ -57,29 +57,30 @@ def nextGeneration():
         nextRow = []
         for column in range(cellCount):
             #we have selected a tile to evaluate
-            bCount = 0
+            aCount = 0
             cell = level[row][column]
             for i in range(1,4):
                 for j in range(1,4):
                     try:
-                        if row+2-i < 0 or column+2-j < 0:
+                        if row+2-i < 0 or column+2-j < 0: #if the function is checking a negative array index, which would be on the opposite side of the level
                             pass
-                        elif i == 2 and j ==2:
+                        elif i == 2 and j ==2: #if the function is checking the tile
                             pass
-                        elif level[row+2-i][column+2-j] == "b":
-                            bCount += 1
+                        elif level[row+2-i][column+2-j] == "a":
+                            aCount += 1
                     except:
                         pass
-            if cell == "b" and bCount in [2,3]:# a = dead, b = alive
-                note = "The cell at "+str(column+1)+"x, "+str(row+1)+"y is alive and will stay alive"
-                nextRow.append("b")
-            elif cell == "b" and bCount not in [2,3]:
-                note = "The cell at "+str(column+1)+"x, "+str(row+1)+"y is alive but will die"
+            if cell == "a" and aCount in [2,3]:# d = dead, a = alive
+                #The cell is alive and will stay alive
                 nextRow.append("a")
-            elif cell == "a" and bCount == 3:
-                note = "The cell at "+str(column+1)+"x, "+str(row+1)+"y is dead but will come alive"
-                nextRow.append("b")
+            elif cell == "a" and aCount not in [2,3]:
+                #The cell is alive but will die
+                nextRow.append("d")
+            elif cell == "d" and aCount == 3:
+                #The cell is dead but will come alive
+                nextRow.append("a")
             else:
+                #the cell is dead and will stay dead
                 nextRow.append(level[row][column])
         nextFrame.append(nextRow)
     return nextFrame
@@ -88,33 +89,39 @@ def createLevel(loops):
     for i in range(loops):
         row = []
         for j in range(loops):
-            row.append("a")
+            row.append("d")
         level.append(row)
     return level
-
-
+def clearLevel():
+    for i in range(cellCount):
+        for j in range(cellCount):
+            level[i][j] = "d"
 
 #main
 pygame.init()
-screenSize = 500
-cellSize = 20
-tickSpeed = 0.1
+screenSize = 1000
+cellSize = 10
 cellCount = screenSize//cellSize
 
+clock=pygame.time.Clock()
 screen = pygame.display.set_mode((screenSize,screenSize))
 click = pygame.mixer.Sound("click.wav")
 
-cellA = cell((40,40,40),(0,0),(cellSize - 1))
-cellB = cell((255,255,255),(0,0),(cellSize -1))
-
+#D = dead, A = alive
+cellD = cell((40,40,40),(0,0),(cellSize - 1))
+cellA = cell((255,255,255),(0,0),(cellSize -1))
+generationTimer = 1
 level = createLevel(cellCount)
 
 ready = False
 while True:
     if ready == True:
-        level = nextGeneration()
-        time.sleep(tickSpeed)
+        generationTimer+=1
+        if generationTimer >= 3:
+            generationTimer = 0
+            level = nextGeneration()
     drawScreen()
     events = pygame.event.get()
     eventHandler()
+    clock.tick(60)
     pygame.display.update()
